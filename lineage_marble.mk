dfe7f9cfd6e9f1c8771a88d70882c19d66eeee5c
kenway214 <kenway214@outlook.com>
2024-06-02 03:49:04 +0530

    marble: Rising-14-ify
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -7,17 +7,26 @@
 # Inherit from marble device
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
-# Inherit from common lineage configuration
+# Inherit from common RisingOS configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_marble
-PRODUCT_DEVICE := marble
-PRODUCT_MANUFACTURER := Xiaomi
+# Device identifier
 PRODUCT_BRAND := POCO
+PRODUCT_DEVICE := marble
 PRODUCT_MODEL := 23049PCD8G
+PRODUCT_NAME := lineage_marble
+PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_SYSTEM_NAME := marble_global
 PRODUCT_SYSTEM_DEVICE := marble
+
+# RisingOS
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_CHIPSET="Snapdragon® 7+ Gen 2" \
+    RISING_MAINTAINER="Phoenix241"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="marble_global-user 14 UKQ1.230804.001 V816.0.3.0.UMRMIXM release-keys" \
